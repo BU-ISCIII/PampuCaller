bf798656183f23340bd0090a6724d9d9cc1995ea
smonzon <smonzon@PC-046614.isciii.es>
2014-06-06 14:35:03 +0200

    Gráfica de frecuencias de nucleótidos en methods del controlset

--- R/methods-controlSet.R
+++ R/methods-controlSet.R
@@ -11,17 +11,20 @@
 
 setMethod("initialize","controlSet",
 			function(.Object,
-					variation = data.frame(),
+					variation = .Object@variation,
 					genes = data.frame(),
 					polymorphisms = data.frame(),
 					samples = data.frame(),
-					... ){
-						.Object<-callNextMethod(.Object,
-						variation = variation ,
-						genes = genes,
-						polymorphisms = polymorphisms,
-						samples = samples,	
-						...)				
+					... ){		
+
+					variation$depth <- variation$A + variation$a + variation$C + 
+									   + variation$c + variation$T + variation$t + variation$G + variation$g + variation$DEL + variation$INS
+					variation$per_A <- (variation$A + variation$a)/variation$depth
+					variation$per_C <- (variation$C + variation$c)/variation$depth
+					variation$per_T <- (variation$T + variation$t)/variation$depth
+					variation$per_G <- (variation$G + variation$g)/variation$depth
+					.Object@variation <- variation
+					return(.Object)
 			}
 )	
 
@@ -58,14 +61,32 @@ setMethod("dim","controlSet",
 ## Features
 ##################
 
-.draw_curve <- function(object,){
+.graph_barerror <- function(object,depth,...){
+	var <- get_variation(object)
+	var <- var[var$depth > depth,]
+	var_ggplot <- melt(var,id.vars=c('sample','POS'), measure.vars=c('per_A','per_G','per_T','per_C'))
 
+	g <-ggplot(var_ggplot, aes(POS,value, fill=variable),scale) +
+ 		geom_bar(stat="identity",position="dodge") + 		
+ 		ylim(0,0.0008) +
+ 		theme(axis.text.x = element_text(size = 5.5,angle=75, vjust=0.5), strip.text.x = element_text(size=6.5)) + 
+ 		scale_fill_manual(values=cbPalette) + 
+ 		theme_bw() + 
+ 		labs(title="Amplicon Position Error", x="POS") +
+ 		facet_wrap(~sample)
+	g
 }
-
-setMethod("draw_curve",signature="controlSet",.draw_curve)
+#scale_y_continuous(trans=log_trans(),breaks=c(0.01,0.1,0.5,0.9,1.0)) +
+setMethod("graph_barerror",signature="controlSet",.graph_barerror)
 
 .mean_sd <- function(object,position,samples,nucleotide){
 
 }
 
 setMethod("mean_sd",signature="controlSet",.mean_sd)
+
+.get_variation <- function(object){
+	variation <- object@variation
+	variation
+}
+setMethod("get_variation",signature="controlSet",.get_variation)
